4de16117c50bb17f7c879bcfe018f213796c8bf3
namanpateriya <pateriyanaman@gmail.com>
2026-05-04 16:36:54 +0530

    Update chunking.py

--- app/chunking.py
+++ app/chunking.py
@@ -1,10 +1,16 @@
 def chunk_text(text, chunk_size=300, overlap=50):
+    text = text.replace("\n", " ").strip()
+
     chunks = []
     start = 0
 
     while start < len(text):
         end = start + chunk_size
-        chunks.append(text[start:end])
+        chunk = text[start:end]
+
+        if chunk:
+            chunks.append(chunk)
+
         start += chunk_size - overlap
 
     return chunks
